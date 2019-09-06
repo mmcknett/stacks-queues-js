ee786ebd48241183461765b7d30359792bf5f821
Matt McKnett <mmcknett@gmail.com>
2019-09-05 23:03:33 -0700

    feature: Create Stack with isEmpty functionality.

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,9 +1,17 @@
 const expect = require('chai').expect;
+const Stack = require('../lib/stack.js');
 
 describe('Stack', function() {
+  describe('constructor', function() {
+    it('is empty when first created.', function() {
+      const stack = new Stack();
+      expect(stack.isEmpty()).to.be.true;
+    });
+  });
+
   describe('push', function() {
     it.skip('is not empty after pushing an object.', function() {
-      expect(false).to.be.true;
+      const stack = new Stack();
     });
   });
 });
