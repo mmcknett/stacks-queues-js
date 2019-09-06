d8e5cf1a9bfa6e21920b65698f7dd95fc434c707
Matt McKnett <mmcknett@gmail.com>
2019-09-05 23:36:23 -0700

    feat: Wave 1, implement a list-based stack

--- test/stack.test.js
+++ test/stack.test.js
@@ -9,9 +9,46 @@ describe('Stack', function() {
     });
   });
 
-  describe('push', function() {
-    it.skip('is not empty after pushing an object.', function() {
+  describe('push & pop', function() {
+    it('is not empty after pushing an object.', function() {
       const stack = new Stack();
+      stack.push('a');
+      expect(stack.isEmpty()).to.be.false;
     });
+
+    it('returns null when popping an empty stack', function () {
+      const stack = new Stack();
+      const value = stack.pop();
+      expect(value).to.be.null;
+    })
+
+    it('returns the value pushed after popping.', function() {
+      const stack = new Stack();
+      stack.push('a');
+      const value = stack.pop();
+      expect(value).to.equal('a');
+    });
+
+    it('is empty after popping a single pushed object.', function() {
+      const stack = new Stack();
+      stack.push('a');
+      stack.pop();
+      expect(stack.isEmpty()).to.be.true;
+    });
+
+    it('returns expected values after popping several objects.', function() {
+      const stack = new Stack();
+
+      stack.push('a');
+      stack.push('b');
+      stack.push('c');
+      const expectC = stack.pop();
+      const expectB = stack.pop();
+      const expectA = stack.pop();
+
+      expect(expectA).to.equal('a');
+      expect(expectB).to.equal('b');
+      expect(expectC).to.equal('c');
+    })
   });
 });
