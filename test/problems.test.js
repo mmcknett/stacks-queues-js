a219ce658c8f24535f8e86587286885077f05f24
Matt McKnett <mmcknett@gmail.com>
2019-09-08 14:26:18 -0700

    test: Add problems test based on ruby tests

---
+++ test/problems.test.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+const { balanced } = require('../lib/problems');
+
+describe("Test wave 3 problems", function() {
+  describe("balanced", function() {
+    it.skip("regards an empty string as balanced", function() {
+      expect(balanced('')).to.be.true;
+    });
+
+    it.skip("Given balanced strings it should return true", function() {
+      expect(balanced('(({}))')).to.be.true;
+    });
+
+    it.skip("will return false for an unbalanced set of parens", function() {
+      expect(balanced('(()')).to.be.false;
+      expect(balanced('(()}')).to.be.false;
+      expect(balanced('([]]')).to.be.false;
+    });
+
+    it.skip("also works for {} and []", function() {
+      expect(balanced('[]')).to.be.true;
+      expect(balanced('{}')).to.be.true;
+    });
+  });
+});
