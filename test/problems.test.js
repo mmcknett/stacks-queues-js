76be5982f2e4122407b48829bd91ac26b9dc595c
Matt McKnett <mmcknett@gmail.com>
2019-09-08 15:13:30 -0700

    Empty string is balanced

--- test/problems.test.js
+++ test/problems.test.js
@@ -3,7 +3,7 @@ const { balanced } = require('../lib/problems');
 
 describe("Test wave 3 problems", function() {
   describe("balanced", function() {
-    it.skip("regards an empty string as balanced", function() {
+    it("regards an empty string as balanced", function() {
       expect(balanced('')).to.be.true;
     });
 
